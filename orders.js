384739bbe7d992eaae0c3cfc078ddb44fc0df644
Manol Trendafilov <manol.trendafilov@gmail.com>
2015-10-16 08:14:42 +0300

    ~ scrape data and save in CSV file

--- orders.js
+++ orders.js
@@ -8,6 +8,7 @@ var casper = require("casper").create({
 });
 
 var fs = require('fs');
+var utils = require('utils');
 
 casper.options.waitTimeout = 10000; 
 
@@ -18,6 +19,7 @@ var user = '', pass = '';
 var currentLink = 1;
 var config = {};
 var orderLinks = [];
+var data = [];
 // load config
 
 configFile = fs.read('config.json');
@@ -34,7 +36,7 @@ function processPage() {
     
     processDetails();
     
-    if(!this.exists("ul.a-pagination li.a-last a") || currentPage > 4)
+    if(!this.exists("ul.a-pagination li.a-last a") || currentPage > 2)
     {
          fetchData();
          casper.then(function(){
@@ -53,29 +55,73 @@ function processPage() {
 function processDetails() 
 {
    orderLinks = casper.evaluate(getLinks).concat(orderLinks);       
-   
-   
-    /*casper.thenOpen(orderLinks[currentLink].href, function() {
-        console.log("------------------ then open ------------------");
-        this.echo(this.getTitle());
-    });
-    */
 }
 
 function fetchData()
 {
     var current = 0;
     var end = orderLinks.length;
+    var tmpCSV = "";
+    var f = fs.open("orders_" + config.email.replace("@", "-") + ".csv", "aw");
+    var csvHeaders = "Full Name, Products, Subtotal, Shipping and Handling, Before Tax, Estimated Tax, Total, Date";
+    f.writeLine(csvHeaders);
+    f.close();
     
     for(;current < end;)
     {
-        console.log(current);
-        
+      
        casper.thenOpen(orderLinks[current], function() {
-                this.capture('screenshots/order-details' + current +'.png');
+                //this.capture('screenshots/order-details' + current +'.png');
+                data = this.evaluate(function() {
+                    
+                    var productTitles = document.querySelectorAll(".shipment .a-box-inner .a-fixed-left-grid-inner .a-col-right a");
+                    productTitlesConcat = "";
+                    for( i = 0; i < productTitles.length; i++ )
+                    {
+                        productTitlesConcat = productTitles[i].textContent.trim();
+                        if(productTitles.length > 1)
+                        {
+                            productTitlesConcat = productTitlesConcat.concat("|", productTitlesConcat);
+                        }
+                        
+                    }
+                    
+                    /*
+                   return {
+                        fullName:     document.querySelector(".displayAddressDiv .displayAddressFullName").textContent,
+                        subtotal:     document.querySelector("#od-subtotals > div:nth-child(2) > div.a-column.a-span4.a-text-right.a-span-last > span").textContent.trim().replace("$", ""),
+                        shipping:     document.querySelector("#od-subtotals > div:nth-child(3) > div.a-column.a-span4.a-text-right.a-span-last > span").textContent.trim().replace("$", ""),
+                        beforeTax:    document.querySelector("#od-subtotals > div:nth-child(5) > div.a-column.a-span4.a-text-right.a-span-last > span").textContent.trim().replace("$", ""),
+                        estimatedTax: document.querySelector("#od-subtotals > div:nth-child(6) > div.a-column.a-span4.a-text-right.a-span-last > span").textContent.trim().replace("$", ""),
+                        total:        document.querySelector("#od-subtotals > div:nth-child(8) > div.a-column.a-span4.a-text-right.a-span-last > span").textContent.trim().replace("$", ""),
+                        date:         new Date(document.querySelector("#orderDetails > div:nth-child(4) > div.a-column.a-span9.a-spacing-top-mini > div > span:nth-child(1)").textContent.replace("Ordered on", "")),
+                        products:     productTitlesConcat
+
+                   } */
+                   return [
+                         document.querySelector(".displayAddressDiv .displayAddressFullName").textContent,
+                         productTitlesConcat,
+                         document.querySelector("#od-subtotals > div:nth-child(2) > div.a-column.a-span4.a-text-right.a-span-last > span").textContent.trim().replace("$", ""),
+                         document.querySelector("#od-subtotals > div:nth-child(3) > div.a-column.a-span4.a-text-right.a-span-last > span").textContent.trim().replace("$", ""),
+                         document.querySelector("#od-subtotals > div:nth-child(5) > div.a-column.a-span4.a-text-right.a-span-last > span").textContent.trim().replace("$", ""),
+                         document.querySelector("#od-subtotals > div:nth-child(6) > div.a-column.a-span4.a-text-right.a-span-last > span").textContent.trim().replace("$", ""),
+                         document.querySelector("#od-subtotals > div:nth-child(8) > div.a-column.a-span4.a-text-right.a-span-last > span").textContent.trim().replace("$", ""),
+                         document.querySelector("#orderDetails > div:nth-child(4) > div.a-column.a-span9.a-spacing-top-mini > div > span:nth-child(1)").textContent.replace("Ordered on", "").trim()
+                        ]
+                   
+                });
+                
+                utils.dump(data.join());
+                
+                tmpCSV = data.join();
+                var f = fs.open("orders_" + config.email.replace("@", "-") + ".csv", "aw");
+                f.writeLine(tmpCSV);
+                f.close();    
         });
         current++;
     }
+    
+    
 }
 
 function getLinks() {
